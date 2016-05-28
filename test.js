4bbe50a748bde5cdf1c022580cd26206d9a8f05d
Kevin Donahue <kevindonahuedevelopment@gmail.com>
2016-05-28 17:34:28 -0400

    add ansiRegex

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 import hasAnsi from 'has-ansi';
+import ansiRegex from 'ansi-regex';
 import test from 'ava';
 import fn from './';
 
-const ec = '\e[1;32m';
+const ec = '\u001b[32m';
 const str = fn(['bold', 'italic', 'green'])('this is bold, italic, and green.');
 
 test('fn prints str with correct ansi code', t => {
+  t.true(str.match(ansiRegex()).indexOf(ec) !== -1);
   t.true(hasAnsi(str));
 })
