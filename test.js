e390c8cd2de84f1d45762d0590b45a05e3281d03
Kevin Donahue <kevindonahuedevelopment@gmail.com>
2016-05-28 13:38:42 -0400

    test tweak

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ import hasAnsi from 'has-ansi';
 import test from 'ava';
 import fn from './';
 
-// const ec = '\e[1;32m';
+const ec = '\e[1;32m';
+const str = fn(['bold', 'italic', 'green'])('this is bold, italic, and green.');
 
 test('fn prints str with correct ansi code', t => {
-  const str = fn(['bold', 'italic', 'green'])('this is bold, italic, and green.');
   t.true(hasAnsi(str));
 })
